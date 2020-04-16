1e081c347ec85d8df74eb8d54467a363122c7333
Josh Maxwell <cctechwiz@gmail.com>
2020-04-16 06:17:49 -0600

    Camera moves diagonally, obstacle scaling and hit boxes adjusted, added boundary hit boxes

--- game/scripts/Camera.gd
+++ game/scripts/Camera.gd
@@ -6,6 +6,7 @@ onready var player: QPlayer = Utils.get_main_node().get_node("Player")
 
 func _physics_process(delta: float) -> void:
 	position.x = player.position.x
+	position.y += 1
 
 
 func get_total_position() -> Vector2:
