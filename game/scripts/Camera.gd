29f475fc565c8a46e573097e0acaf72221bd5dab
Josh Maxwell <cctechwiz@gmail.com>
2020-04-16 06:08:02 -0600

    Player move better, and dummy objects are in main stage

---
+++ game/scripts/Camera.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+class_name QCamera
+
+onready var player: QPlayer = Utils.get_main_node().get_node("Player")
+
+
+func _physics_process(delta: float) -> void:
+	position.x = player.position.x
+
+
+func get_total_position() -> Vector2:
+	return position + offset
--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
+class_name QPlayer
+
 
 var speed: int = 100
-var gravity: int = 400
-var swim_speed: int = -500
-var velocity: Vector2 = Vector2.ZERO #Vector2(1, 2)
+var gravity: int = 1500
+var swim_direction: Vector2 = Vector2(3, 4)
+var swim_velocity: int = -500
+var velocity: Vector2 = Vector2()
 
 
 func _ready() -> void:
@@ -12,24 +15,24 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("swim"):
-		#swim()
-		pass
+		print("swim")
+		velocity.y = swim_velocity
 
 
-func get_input() -> void:
+func _get_input() -> void:
 	if Input.is_action_pressed("swim"):
 		swim()
 		pass
 
 
 func _physics_process(delta: float) -> void:
+	_get_input()
 	# velocity = velocity.normalized() * speed
-	# velocity.x = speed
-	velocity.y = gravity
-	get_input()
-	move_and_collide(velocity * delta)
+	velocity.x = speed
+	velocity.y += gravity * delta
+
+	velocity = move_and_slide(velocity)
 
 
 func swim() -> void:
-	print("swim")
-	velocity.y = swim_speed
+	pass
